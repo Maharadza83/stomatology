59dbd2fb1b7209a8e3e4f57f7d96d5384b5e1b3d
Dominik <lolkowskid@gmail.com>
2024-07-29 16:34:41 +0200

    carousel

--- nfz_ang/src/app/core/home/components/doctors-carousel/components/doctors-carousel.animate.ts
+++ nfz_ang/src/app/core/home/components/doctors-carousel/components/doctors-carousel.animate.ts
@@ -2,12 +2,15 @@ import { animate, query, stagger, style, transition, trigger } from '@angular/an
 
 export const DOCTORS_CAROUSEL_ANIMATION = trigger('doctorsCarouselAnimation', [
   transition('* => *', [
+    // Animate entering elements
     query(':enter', [
       style({ opacity: 0, transform: 'translateX(100%)' }),
       stagger('100ms', [
         animate('500ms ease-out', style({ opacity: 1, transform: 'translateX(0)' })),
       ]),
     ], { optional: true }),
+
+    // Animate leaving elements
     query(':leave', [
       stagger('100ms', [
         animate('500ms ease-out', style({ opacity: 0, transform: 'translateX(-100%)' })),
